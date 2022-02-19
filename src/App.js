0f91d77dfc67cc47813bdfe0617a9cf539896850
valeavienna <valeria.corso@ymail.com>
2022-02-19 11:25:35 +0100

    first deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header"></header>
+      <Dictionary />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Dictionary.css";
+
+function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default Dictionary;

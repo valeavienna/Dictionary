7a02d318eef6eee7735a9ebe5fa763fa6ea92745
valeavienna <valeria.corso@ymail.com>
2022-02-19 12:55:56 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App container">
       <header className="App-header"></header>
       <Dictionary />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="Footer">
+      <a
+        href="https://github.com/valeavienna/Dictionary"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-github white"></i>Coded by Valeria Corso
+      </a>
+    </div>
+  );
+}
+export default Footer;

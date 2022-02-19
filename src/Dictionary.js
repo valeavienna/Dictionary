eb27fb3ca7b6f82109c4097d3607416b5a5bf4c4
valeavienna <valeria.corso@ymail.com>
2022-02-19 11:53:21 +0100

    integrated api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 function Dictionary() {
+  let [word, setWord] = useState(null);
+  let [definition, setDefinition] = useState([{}]);
+  let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  function search(event) {
+    event.preventDefault();
+    axios.get(apiURL).then(response);
+  }
+
+  function handleChange(event) {
+    setWord(event.target.value);
+  }
+
+  function response(response) {
+    setDefinition(response.data[0]);
+    console.log(definition);
+  }
+
   return (
     <div className="Dictionary">
-      <h1>Hello</h1>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleChange}></input>
+        <input type="submit" value="Search"></input>
+      </form>
     </div>
   );
 }
